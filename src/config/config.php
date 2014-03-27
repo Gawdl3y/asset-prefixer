612797ae838c00bfe1e57d72df5a66cf951725fe
Schuyler Cebulskie <Gawdl3y@gmail.com>
2014-03-27 15:00:37 -0400

    Added config file

---
+++ src/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Prefix
+	|--------------------------------------------------------------------------
+	|
+	| This is the prefix that will be added to asset paths when using the
+	| AssetBuilder or the Asset Facade.
+	|
+	*/
+
+	'prefix' => null,
+
+);
